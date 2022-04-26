46941a4c7c16f94af09704197e5bcf6cca227991
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-04-26 17:22:49 +0300

    added days digit

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -4,6 +4,8 @@ import Separator from './Separator'
 function Clock() {
   return (
     <div className="flex flex-row justify-center font-thin uppercase text-center text-slate-600 text-5xl mb-5 mt-5">
+      <Digit digit="3" />
+      <Separator />
       <Digit digit="15" />
       <Separator />
       <Digit digit="03" />
