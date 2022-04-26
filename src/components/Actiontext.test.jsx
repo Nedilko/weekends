5286625ce7243dc0e2074201d0b52132121abb8f
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-04-27 02:05:28 +0300

    added some tests

--- src/components/Actiontext.test.jsx
+++ src/components/Actiontext.test.jsx
@@ -2,7 +2,7 @@ import Actiontext from './Actiontext'
 import { render, screen } from '../utils/test-utils'
 
 describe('text in the document', () => {
-  it('title is visible', () => {
+  it('action text is visible', () => {
     render(<Actiontext />)
     const title = screen.getByText(/Time left to weekends/i)
     expect(title).toBeInTheDocument()
--- src/components/Greetings.test.jsx
+++ src/components/Greetings.test.jsx
@@ -1,8 +1,8 @@
 import Greetings from './Greetings'
 import { render, screen } from '../utils/test-utils'
 
-describe('text in the document', () => {
-  it('title is visible', () => {
+describe('greeting in the document', () => {
+  it('text is visible', () => {
     render(<Greetings />)
     const title = screen.getByText(/Have a beer/i)
     expect(title).toBeInTheDocument()
