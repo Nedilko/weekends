a21d6f9090508e04b37dd1e9db933c7ca53ca59b
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-04-29 02:17:33 +0300

    added animation

--- src/components/SettingsIcon.jsx
+++ src/components/SettingsIcon.jsx
@@ -7,7 +7,7 @@ SettingsIcon.propTypes = {
 
 function SettingsIcon({ onClick }) {
   return (
-    <div className="flex w-12 h-12 items-center justify-center flex-shrink-0 p-2 rounded-full">
+    <div className="flex w-12 h-12 items-center justify-center flex-shrink-0 p-2 rounded-full duration-300 ease-in-out hover:rotate-45">
       <CogIcon
         onClick={onClick}
         className="flex w-8 h-8 absolute rounded-full text-slate-500 dark:text-gray-600"
