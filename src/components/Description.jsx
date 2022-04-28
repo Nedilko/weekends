842417aa0ba16538241a8629c6c298652b0d31e0
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-04-28 10:55:45 +0300

    cganged margin-top for landscape

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,6 +1,6 @@
 function Description() {
   return (
-    <h1 className="font-thin uppercase text-center text-slate-600 dark:text-gray-400 text-5xl mb-10 mt-40">
+    <h1 className="font-thin uppercase text-center text-slate-600 dark:text-gray-400 text-5xl mb-10 mt-40 landscape:mt-20">
       weekends countdown
     </h1>
   )
