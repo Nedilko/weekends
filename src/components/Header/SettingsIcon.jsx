f138cba87d7e4cc5d20853cbc03cc6d93042d73c
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-04-29 21:51:28 +0300

    removed library component

--- src/components/Header/SettingsIcon.jsx
+++ src/components/Header/SettingsIcon.jsx
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types'
-import { CogIcon } from '@heroicons/react/outline'
 
 SettingsIcon.propTypes = {
   onClick: PropTypes.func,
@@ -8,10 +7,26 @@ SettingsIcon.propTypes = {
 function SettingsIcon({ onClick }) {
   return (
     <div className="flex w-12 h-12 items-center justify-center flex-shrink-0 p-2 rounded-full duration-300 ease-in-out hover:rotate-45">
-      <CogIcon
-        onClick={onClick}
+      <svg
         className="flex w-8 h-8 absolute rounded-full text-slate-500 dark:text-gray-600"
-      />
+        onClick={onClick}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth="2"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+        />
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+        />
+      </svg>
     </div>
   )
 }
