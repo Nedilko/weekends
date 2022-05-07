8cdff4ac0acb3d0bae353efcc39106234aaa5389
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-07 13:47:24 +0000

    reconfigured theme toggle

--- src/components/UI/Toggles/ToggleTheme.jsx
+++ src/components/UI/Toggles/ToggleTheme.jsx
@@ -2,38 +2,36 @@ import PropTypes from 'prop-types'
 import { MoonIcon, SunIcon } from '@heroicons/react/outline'
 
 ToggleTheme.propTypes = {
-  label: PropTypes.string,
   checked: PropTypes.bool.isRequired,
   onClick: PropTypes.func.isRequired,
 }
 
-function ToggleTheme({ label, checked, onClick }) {
+function ToggleTheme({ checked, onClick }) {
   return (
-    <label className="flex items-center p-2 text-xl font-thin text-slate-600 dark:text-gray-400">
-      {label}
+    <div className="flex justify-center items-center">
       <span
         onClick={onClick}
-        className={`flex justify-between w-16 h-8 items-center flex-shrink-0 ml-4 p-1 rounded-full ${
+        className={`flex justify-between items-center w-16 h-8 flex-shrink-0 rounded-full ${
           checked ? 'bg-slate-700' : 'bg-gray-200'
         }`}
       >
         <MoonIcon
-          className={`w-5 h-5 ml-1 rounded-full text-yellow-500 duration-300 ease-in-out ${
+          className={`w-5 h-5 ml-1.5 rounded-full text-yellow-500 duration-300 ease-in-out ${
             checked ? '' : 'opacity-0 scale-0'
           }`}
         />
         <SunIcon
-          className={`w-5 h-5 mr-1 rounded-full text-yellow-500 duration-300 ease-in-out ${
+          className={`w-5 h-5 mr-1.5 rounded-full text-yellow-500 duration-300 ease-in-out ${
             checked ? 'opacity-0 scale-0' : ''
           }`}
         />
         <span
-          className={`absolute flex h-7 w-7 rounded-full text-yellow-500 p-1 shadow-md duration-300 ease-in-out ${
-            checked ? 'translate-x-7 bg-slate-900' : 'bg-white'
+          className={`absolute flex h-7 w-7 ml-0.5 rounded-full text-yellow-500 shadow-md duration-300 ease-in-out ${
+            checked ? 'translate-x-8 bg-slate-900' : 'bg-white'
           }`}
         ></span>
       </span>
-    </label>
+    </div>
   )
 }
 
