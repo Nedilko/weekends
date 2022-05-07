1e1c0fc709236889de9b5b3754ad47fa1d1414c0
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-07 19:09:25 +0000

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer className="font-thin text-base text-center mb-2">
+      <a target="_blank" rel="noreferrer" href="https://nac.in.ua">
+        nac.in.ua
+      </a>
+    </footer>
+  )
+}
+
+export default Footer
