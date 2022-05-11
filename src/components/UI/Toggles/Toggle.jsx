a6c021fca8e79979ed79455faae01fa27edfee72
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-11 10:47:58 +0000

    changed toggle dimentions

--- src/components/UI/Toggles/Toggle.jsx
+++ src/components/UI/Toggles/Toggle.jsx
@@ -18,19 +18,19 @@ function Toggle({ label, checked = false, onChange }) {
   }, [isChecked])
 
   return (
-    <label className="flex items-center p-2 text-xl font-thin text-slate-600 dark:text-gray-400">
+    <label className="flex items-center px-1 text-xl font-thin text-slate-600 dark:text-gray-400">
       {label}
       <span
         onClick={handleClick}
-        className={`flex justify-between w-16 h-8 items-center flex-shrink-0 ml-4 p-1 rounded-full duration-300 ease-in-out ${
+        className={`ml-4 flex h-6 w-12 flex-shrink-0 items-center justify-between rounded-full px-1 duration-300 ease-in-out ${
           isChecked
             ? 'bg-green-400 dark:bg-green-600'
             : 'bg-gray-200 dark:bg-slate-700'
         }`}
       >
         <span
-          className={`absolute flex h-7 w-7 rounded-full text-yellow-500 p-1 shadow-md bg-white dark:bg-slate-900 duration-300 ease-in-out ${
-            isChecked ? 'translate-x-7' : ''
+          className={`absolute flex h-5 w-5 rounded-full bg-white text-yellow-500 shadow-md duration-300 ease-in-out dark:bg-slate-900 ${
+            isChecked ? 'translate-x-5' : ''
           }`}
         ></span>
       </span>
