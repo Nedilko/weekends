2f710ea0f5366d8621315623a3a202667fcdddcc
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-12 23:57:05 +0300

    greetings text validation (#103)
    * small UI adjustements
    
    * implemented validation UI information

--- src/components/UI/Buttons/Button.jsx
+++ src/components/UI/Buttons/Button.jsx
@@ -8,7 +8,7 @@ Button.propTypes = {
 function Button({ onClick, title }) {
   return (
     <button
-      className="mx-4 w-16 py-2 text-xl font-thin text-slate-600 duration-200 ease-in-out hover:scale-110 focus:font-light focus:outline-none dark:text-gray-400"
+      className="mx-4 w-16 py-2 text-xl font-thin text-slate-600 duration-200 ease-in-out hover:scale-110 focus:scale-110 focus:outline-none dark:text-gray-400"
       onClick={onClick}
     >
       {title}
--- src/components/UI/Inputs/TextInput.jsx
+++ src/components/UI/Inputs/TextInput.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types'
+import { useRef, useState } from 'react'
 
 TextInput.propTypes = {
   placeholder: PropTypes.string,
@@ -7,16 +8,29 @@ TextInput.propTypes = {
 }
 
 function TextInput({ placeholder, value, onChange }) {
+  const ref = useRef(null)
+  const [isValid, setIsValid] = useState(true)
+  const handleChange = (e) => {
+    if (onChange) {
+      onChange(e.target.value)
+    }
+    if (ref.current.value === '') {
+      setIsValid(false)
+    } else {
+      setIsValid(true)
+    }
+  }
   return (
     <input
+      ref={ref}
       type="text"
       placeholder={placeholder}
       maxLength="20"
       value={value}
-      onChange={(e) => {
-        onChange(e.target.value)
-      }}
-      className="mx-2 flex rounded-md border border-slate-100 px-2 text-xl font-thin text-slate-600 focus:border-slate-300 focus:outline-none dark:border-gray-700 dark:bg-zinc-900 dark:text-gray-400 dark:focus:border-gray-500"
+      onChange={handleChange}
+      className={`${
+        !isValid ? 'border-red-500' : 'border-slate-100 focus:border-slate-300'
+      } mx-2 flex rounded-md border px-2 text-xl font-thin text-slate-600 focus:outline-none dark:border-gray-700 dark:bg-zinc-900 dark:text-gray-400 dark:focus:border-gray-500`}
     />
   )
 }
