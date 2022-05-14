e123484ea0664792f839a4f056d37a7ca6f7960f
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-14 22:18:32 +0300

    refactoring (#108)

--- src/hooks/useCountdown.js
+++ src/hooks/useCountdown.js
@@ -2,18 +2,17 @@ import { useState, useEffect } from 'react'
 import { getTimeLeft } from '@utils/getTimeLeft'
 
 const useCountdown = (targetTime) => {
-  const timeLeft = getTimeLeft(targetTime)
-  const [time, setTime] = useState(timeLeft)
+  const [time, setTime] = useState(getTimeLeft(targetTime))
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTime(timeLeft)
+      setTime(getTimeLeft(targetTime))
     }, 1000)
 
     return () => {
       clearInterval(interval)
     }
-  }, [timeLeft])
+  }, [time])
 
   return time
 }
