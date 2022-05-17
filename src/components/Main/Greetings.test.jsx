3dc689db16f06e517b8d37704f6684e4d42c562e
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-17 22:37:32 +0000

    greetings text

---
+++ src/components/Main/Greetings.test.jsx
@@ -0,0 +1,16 @@
+import Greetings from '@components/Main/Greetings'
+import SettingsContext from '@store/Settings'
+import { render, screen } from '@utils/test-utils'
+
+describe('greetings text in the document', () => {
+  it('is visible', () => {
+    render(
+      <SettingsContext.Provider value={{ data: { greetingsText: 'sample' } }}>
+        <Greetings />
+      </SettingsContext.Provider>
+    )
+    const title = screen.getByText(/sample/i)
+    expect(title).toBeInTheDocument()
+    expect(title).toMatchSnapshot()
+  })
+})
