15b05a5dd2b0dd237f8e2d412a941627e2ef8ce1
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-17 22:17:18 +0000

    description

--- src/components/Main/Description.test.jsx
+++ src/components/Main/Description.test.jsx
@@ -1,10 +1,11 @@
-import Description from './Description'
-import { render, screen } from '../../utils/test-utils'
+import Description from '@components/Main/Description'
+import { render, screen } from '@utils/test-utils'
 
 describe('text in the document', () => {
   it('title is visible', () => {
     render(<Description />)
-    const title = screen.getByText(/weekends countdown/i)
+    const title = screen.getByRole('heading')
     expect(title).toBeInTheDocument()
+    expect(title).toMatchSnapshot()
   })
 })
