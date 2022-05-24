9d93b031b776df481bb8eb035c186dc77cbcb11c
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-24 15:15:00 +0000

    added header tests

---
+++ src/components/Header/Header.test.jsx
@@ -0,0 +1,18 @@
+import Header from '@components/Header/Header'
+import SettingsContext from '@store/Settings'
+import { render, vi } from '@utils/test-utils'
+
+describe('DarkModeSwitcher', () => {
+  const handleApply = vi.fn()
+
+  it('should be in the document', () => {
+    const { container } = render(
+      <SettingsContext.Provider
+        value={{ data: { useSystemTheme: false, theme: 'light' }, handleApply }}
+      >
+        <Header />
+      </SettingsContext.Provider>
+    )
+    expect(container).toMatchSnapshot()
+  })
+})
