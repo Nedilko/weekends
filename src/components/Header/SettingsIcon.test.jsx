4386c4fe921eb2251c5ecf33d978046f2ba955c0
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-24 15:08:08 +0000

    refactoring

--- src/components/Header/SettingsIcon.test.jsx
+++ src/components/Header/SettingsIcon.test.jsx
@@ -1,8 +1,8 @@
 import SettingsIcon from '@components/Header/SettingsIcon'
 import { render, screen } from '@utils/test-utils'
 
-describe('settings icon is', () => {
-  it('in the document', () => {
+describe('settings', () => {
+  it('should render', () => {
     render(<SettingsIcon />)
     const icon = screen.getByRole('img')
     expect(icon).toBeInTheDocument()
