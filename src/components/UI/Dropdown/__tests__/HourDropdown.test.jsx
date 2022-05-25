668b0bc2d08b8545b9506553f058a2b9d8bfee81
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-25 12:32:26 +0000

    added hour dropdown integrations tests

---
+++ src/components/UI/Dropdown/__tests__/HourDropdown.test.jsx
@@ -0,0 +1,45 @@
+import HourDropdown from '@UI/Dropdown/HourDropdown'
+import { render, vi, userEvent } from '@utils/test-utils'
+import { screen } from '@testing-library/react'
+
+describe('HourDropdown', () => {
+  beforeAll(() => {
+    window.HTMLElement.prototype.scrollIntoView = vi.fn()
+  })
+
+  afterEach(() => {
+    window.HTMLElement.prototype.scrollIntoView.mockClear()
+  })
+
+  it('should render closed', () => {
+    const handleChage = vi.fn()
+    const selectedValue = 3
+    const { container } = render(
+      <HourDropdown onChange={handleChage} selectedValue={selectedValue} />
+    )
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should render with label Day', () => {
+    const handleChage = vi.fn()
+    const selectedValue = 3
+    render(
+      <HourDropdown onChange={handleChage} selectedValue={selectedValue} />
+    )
+    const label = screen.getByText('Hour')
+    expect(label).toBeInTheDocument()
+  })
+
+  it('should trigger on change handler', async () => {
+    const handleChage = vi.fn()
+    const selectedValue = 3
+    const user = userEvent.setup()
+    render(
+      <HourDropdown onChange={handleChage} selectedValue={selectedValue} />
+    )
+    const dropdownElement = screen.getByRole('button')
+    await user.click(dropdownElement)
+    expect(handleChage).toBeCalledTimes(1)
+    expect(handleChage).toBeCalledWith(3)
+  })
+})
