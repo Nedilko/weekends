5d549d2db223046d5b9460bbb45402f226a287f7
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-27 21:11:38 +0000

    enhanced tests

--- src/utils/__tests__/localstorageAdapter.test.js
+++ src/utils/__tests__/localstorageAdapter.test.js
@@ -4,62 +4,63 @@ import {
 } from '@utils/localstorageAdapter'
 
 describe('localstorage adapter', () => {
+  let setItemSpy, getItemSpy
   beforeAll(() => {
-    Object.defineProperty(window, 'localStorage', {
-      writable: true,
-      value: {
-        setItem: vi.fn(),
-        getItem: vi.fn(() => 'sample'),
-      },
-    })
+    setItemSpy = vi.spyOn(global.Storage.prototype, 'setItem')
     vi.spyOn(console, 'error').mockImplementation(() => {})
+    getItemSpy = vi.spyOn(global.Storage.prototype, 'getItem')
+  })
+
+  beforeEach(() => {
+    setItemSpy.mockImplementation(() => {})
+    getItemSpy.mockImplementation(() => 'sample')
   })
 
   afterAll(() => {
     console.error.mockRestore()
+    getItemSpy.mockRestore()
+    setItemSpy.mockRestore()
   })
 
   afterEach(() => {
-    window.localStorage.setItem.mockClear()
-    window.localStorage.getItem.mockClear()
+    getItemSpy.mockClear()
+    setItemSpy.mockClear()
     console.error.mockClear()
   })
 
   it('should read settings from local storage', () => {
-    const getItem = window.localStorage.getItem
     const data = readFromLocalstorage('settings')
-    expect(getItem).toHaveBeenCalled()
-    expect(getItem).toHaveBeenCalledWith('settings')
+    expect(getItemSpy).toHaveBeenCalled()
+    expect(getItemSpy).toHaveBeenCalledWith('settings')
     expect(data).toMatch('sample')
   })
 
   it('should write settings to local storage', () => {
-    const setItem = window.localStorage.setItem
     const data = { day: 2, hour: 3 }
     writeToLocalstorage('settings', data)
-    expect(setItem).toHaveBeenCalled()
-    expect(setItem).toHaveBeenCalledWith('settings', { day: 2, hour: 3 })
+    expect(setItemSpy).toHaveBeenCalled()
+    expect(setItemSpy).toHaveBeenCalledWith('settings', { day: 2, hour: 3 })
   })
 
   it('should throw error reading from localstorage', () => {
-    const getItem = window.localStorage.getItem.mockImplementation(() => {
+    getItemSpy.mockImplementation(() => {
       throw 'error message'
     })
     const data = readFromLocalstorage('settings')
-    expect(getItem).toHaveBeenCalled()
-    expect(getItem).toHaveBeenCalledWith('settings')
+    expect(getItemSpy).toHaveBeenCalled()
+    expect(getItemSpy).toHaveBeenCalledWith('settings')
     expect(data).toBeUndefined()
     expect(console.error).toHaveBeenCalledOnce()
   })
 
   it('should throw error writing to localstorage', () => {
-    const setItem = window.localStorage.setItem.mockImplementation(() => {
+    setItemSpy.mockImplementation(() => {
       throw 'error message'
     })
     const data = { day: 2, hour: 3 }
     writeToLocalstorage('settings', data)
-    expect(setItem).toHaveBeenCalled()
-    expect(setItem).toHaveBeenCalledWith('settings', { day: 2, hour: 3 })
+    expect(setItemSpy).toHaveBeenCalled()
+    expect(setItemSpy).toHaveBeenCalledWith('settings', { day: 2, hour: 3 })
     expect(console.error).toHaveBeenCalledOnce()
   })
 })
