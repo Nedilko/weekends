e1b2c9e946762ce7d3ce56645f78eee68dd31851
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-28 10:54:12 +0000

    refactoring

--- src/components/Footer/__tests__/Footer.test.jsx
+++ src/components/Footer/__tests__/Footer.test.jsx
@@ -1,14 +1,14 @@
 import Footer from '@components/Footer/Footer'
 import { render, screen } from '@utils/test-utils'
 
-describe('footer link', () => {
-  it('is visible', () => {
+describe('Footer', () => {
+  it('should render', () => {
     render(<Footer />)
     const footerLink = screen.getByRole('link')
     expect(footerLink).toBeInTheDocument()
     expect(footerLink).toMatchSnapshot()
   })
-  it('has href', () => {
+  it('should has href', () => {
     render(<Footer />)
     const footerLink = screen.getByRole('link')
     const footerLinkHref = footerLink.getAttribute('href')
