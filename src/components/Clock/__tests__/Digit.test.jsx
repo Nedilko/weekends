479cbd1045b55f79f5b4500c09b6d47db485b97b
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-28 10:40:21 +0000

    refactoring

--- src/components/Clock/__tests__/Digit.test.jsx
+++ src/components/Clock/__tests__/Digit.test.jsx
@@ -1,8 +1,8 @@
 import Digit from '@components/Clock/Digit'
 import { render, screen } from '@utils/test-utils'
 
-describe('clock digit', () => {
-  it('is visible', () => {
+describe('Digit', () => {
+  it('should render digit', () => {
     render(<Digit digit={12} />)
     const title = screen.getByText(/12/i)
     expect(title).toBeInTheDocument()
