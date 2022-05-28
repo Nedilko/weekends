3f9fa7bcc5d5070949b8ce1b955bfbadc0f61d6b
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-28 17:38:31 +0000

    refactoring

--- src/components/Main/__tests__/Description.test.jsx
+++ src/components/Main/__tests__/Description.test.jsx
@@ -1,8 +1,8 @@
 import Description from '@components/Main/Description'
 import { render, screen } from '@utils/test-utils'
 
-describe('description text', () => {
-  it('is visible', () => {
+describe('Description', () => {
+  it('should render', () => {
     render(<Description />)
     const title = screen.getByRole('heading')
     expect(title).toBeInTheDocument()
