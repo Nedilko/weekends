4c763c22984e3ff160f510461fb09c075269b4e2
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-29 15:26:22 +0000

    added test for time util

---
+++ src/utils/__tests__/getTimeLeft.test.js
@@ -0,0 +1,49 @@
+import { getTimeLeft } from '@utils/getTimeLeft'
+
+describe('getTimeLeft', () => {
+  beforeEach(() => {
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
+  it('should calculate time difference', () => {
+    const date = new Date(2022, 4, 23, 11, 12, 11, 0)
+    vi.setSystemTime(date)
+
+    const targetTime = {
+      day: 5,
+      hour: 18,
+      minute: 0,
+      second: 0,
+    }
+    const timeLeft = getTimeLeft(targetTime)
+    expect(timeLeft).toMatchObject({
+      days: 4,
+      hours: 6,
+      minutes: 47,
+      seconds: 49,
+    })
+  })
+
+  it('should return time difference if no time left', () => {
+    const date = new Date(2022, 4, 27, 18, 12, 11, 0)
+    vi.setSystemTime(date)
+
+    const targetTime = {
+      day: 5,
+      hour: 18,
+      minute: 0,
+      second: 0,
+    }
+    const timeLeft = getTimeLeft(targetTime)
+    expect(timeLeft).toMatchObject({
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    })
+  })
+})
