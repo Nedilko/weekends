665f8318081260e56478356bd88da4da7b11fc90
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-31 23:47:50 +0000

    enhanced tests for Panel

--- src/components/UI/Panels/__tests__/Panel.test.jsx
+++ src/components/UI/Panels/__tests__/Panel.test.jsx
@@ -1,11 +1,15 @@
 import Panel from '@UI/Panels/Panel'
 import { render, screen } from '@utils/test-utils'
 
-describe('panel', () => {
-  it('renders text', () => {
-    const { container } = render(<Panel>some text</Panel>)
-    expect(screen.getByText('some text')).toBeInTheDocument()
-    expect(container).toBeInTheDocument()
+describe('Panel', () => {
+  it('should render content', () => {
+    const { container } = render(<Panel>some content</Panel>)
+    expect(screen.getByText('some content')).toBeInTheDocument()
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should render with size', () => {
+    const { container } = render(<Panel size="md">some content</Panel>)
     expect(container).toMatchSnapshot()
   })
 })
