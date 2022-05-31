aa2bd94f96d135e04813a3322efb3fb825b07aea
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-31 20:22:34 +0000

    enhanced tests for DropdownItem

--- src/components/UI/Dropdown/__tests__/DropdownItemsList.test.jsx
+++ src/components/UI/Dropdown/__tests__/DropdownItemsList.test.jsx
@@ -1,45 +1,72 @@
 import DropdownItemsList from '@UI/Dropdown/DropdownItemsList'
 import { render, screen, userEvent } from '@utils/test-utils'
 
-describe('button', () => {
-  const items = ['first', 'second', 'third']
-  beforeAll(() => {
-    window.HTMLElement.prototype.scrollIntoView = vi.fn()
+describe('DropdownItemsList', () => {
+  const selectHandler = vi.fn()
+
+  vi.mock('@UI/Dropdown/DropdownItem', () => {
+    return {
+      default: ({ value, handleSelect }) => (
+        <div data-testid="dropdown-item" onClick={() => handleSelect(value)}>
+          {value}
+        </div>
+      ),
+    }
   })
 
-  afterEach(() => {
-    window.HTMLElement.prototype.scrollIntoView.mockClear()
+  beforeEach(() => {
+    selectHandler.mockClear()
+  })
+
+  it('should render dropdown items list', () => {
+    const selectedValue = 'second'
+    const { container } = render(
+      <DropdownItemsList
+        items={['first']}
+        handleSelect={selectHandler}
+        selectedValue={selectedValue}
+      />
+    )
+    expect(container).toMatchSnapshot()
   })
 
   it('should render 3 dropdown items', () => {
-    const handleSelect = vi.fn()
     const selectedValue = 'second'
     render(
       <DropdownItemsList
-        items={items}
-        handleSelect={handleSelect}
+        items={['first', 'second', 'third']}
+        handleSelect={selectHandler}
         selectedValue={selectedValue}
       />
     )
-    const dropdownItemsList = screen.getAllByTestId('dropdown-item')
-    expect(dropdownItemsList).toHaveLength(3)
-    expect(dropdownItemsList).toMatchSnapshot()
+    expect(screen.getAllByTestId('dropdown-item')).toHaveLength(3)
   })
 
   it('should handle select', async () => {
-    const handleSelect = vi.fn()
     const selectedValue = 'second'
+    const user = userEvent.setup()
     render(
       <DropdownItemsList
-        items={items}
-        handleSelect={handleSelect}
+        items={['first', 'second', 'third']}
+        handleSelect={selectHandler}
         selectedValue={selectedValue}
       />
     )
+    await user.click(screen.getByText('third'))
+    expect(selectHandler).toHaveBeenCalledOnce()
+  })
+
+  it('should handle select with dropdown item value', async () => {
+    const selectedValue = 'second'
     const user = userEvent.setup()
-    const dropdownItem = screen.getByText('third')
-    await user.click(dropdownItem)
-    expect(handleSelect).toHaveBeenCalledTimes(1)
-    expect(handleSelect).toHaveBeenCalledWith('third')
+    render(
+      <DropdownItemsList
+        items={['first', 'second', 'third']}
+        handleSelect={selectHandler}
+        selectedValue={selectedValue}
+      />
+    )
+    await user.click(screen.getByText('third'))
+    expect(selectHandler).toHaveBeenCalledWith('third')
   })
 })
