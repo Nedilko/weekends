cf80eaa9633fd237eff60e8bf554f837421d87e9
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-05-31 17:01:54 +0000

    refactoring footer

--- src/components/Footer/__tests__/Footer.test.jsx
+++ src/components/Footer/__tests__/Footer.test.jsx
@@ -2,18 +2,16 @@ import Footer from '@components/Footer/Footer'
 import { render, screen } from '@utils/test-utils'
 
 describe('Footer', () => {
-  it('should render', () => {
+  it('should render link with text', () => {
     render(<Footer />)
     const footerLink = screen.getByRole('link')
     expect(footerLink).toBeInTheDocument()
     expect(footerLink).toMatchSnapshot()
   })
-  it('should has href', () => {
+  it('should has attributes', () => {
     render(<Footer />)
     const footerLink = screen.getByRole('link')
-    const footerLinkHref = footerLink.getAttribute('href')
-    const footerLinkTarget = footerLink.getAttribute('target')
-    expect(footerLinkHref).toBe('https://nac.in.ua')
-    expect(footerLinkTarget).toBe('_blank')
+    expect(footerLink).toHaveAttribute('href', 'https://nac.in.ua')
+    expect(footerLink).toHaveAttribute('target', '_blank')
   })
 })
