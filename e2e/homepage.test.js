5280bbc0bb79daad2b7da8868db2a124ea34d86c
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-06-06 04:04:26 +0300

    configured

--- e2e/homepage.test.js
+++ e2e/homepage.test.js
@@ -1,8 +1,11 @@
 const { test, expect } = require('@playwright/test')
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://weekends.nac.in.ua')
+})
+
 test.describe('main text', () => {
   test('heading has text', async ({ page }) => {
-    await page.goto('/')
     const title = page.locator('h1')
     await expect(title).toHaveText('weekends countdown')
   })
--- playwright.config.js
+++ playwright.config.js
@@ -56,6 +56,9 @@ const config = {
     url: DEV_SERVER_URL,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
+    waitOnScheme: {
+      delay: 1000,
+    },
   },
 }
 
