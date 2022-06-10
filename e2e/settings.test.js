fc5eadede5ae519ff40204f8f4bcccaa0d887042
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-06-10 23:24:53 +0300

    added settings e2e tests

--- e2e/settings.test.js
+++ e2e/settings.test.js
@@ -1,12 +1,11 @@
 const { test, expect } = require('@playwright/test')
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://weekends.nac.in.ua')
+  await page.goto('/')
 })
 
-test.describe('settings modal', () => {
+test.describe('Verify that user can use settings modal', () => {
   test('should be used by user', async ({ page }) => {
-    await page.locator('text=Apply').click()
     await page.locator('svg').first().click()
     await page.locator('[placeholder="Have a beer"]').click()
     await page.locator('[placeholder="Have a beer"]').fill('some beer!')
@@ -16,5 +15,131 @@ test.describe('settings modal', () => {
     await page.locator('text=00:00').click()
     expect(page.locator('text=Wednesday')).toBeTruthy()
     expect(page.locator('text=00:00')).toBeTruthy()
+  })
+
+  test('should be able to click each day', async ({ page }) => {
+    await page.locator('svg').first().click()
+
+    await page.locator('text=Friday').click()
+    await page.locator('text=Monday').click()
+
+    await page.locator('text=Monday').click()
+    await page.locator('text=Tuesday').click()
+
+    await page.locator('text=Tuesday').click()
+    await page.locator('text=Wednesday').click()
+
+    await page.locator('text=Wednesday').click()
+    await page.locator('text=Thursday').click()
+
+    await page.locator('text=Thursday').click()
+    await page.locator('text=Friday').click()
+
+    await page.locator('text=Friday').click()
+    await page.locator('text=Saturday').click()
+
+    await page.locator('text=Saturday').click()
+    await page.locator('text=Sunday').click()
+
+    await page.locator('text=Sunday').click()
+    await page.locator('text=Friday').click()
+  })
+
+  test('should be able to click each hour', async ({ page }) => {
+    await page.locator('svg').first().click()
+
+    await page.locator('text=18:00').click()
+    await page.locator('text=00:00').click()
+
+    await page.locator('text=00:00').click()
+    await page.locator('text=01:00').click()
+
+    await page.locator('text=01:00').click()
+    await page.locator('text=02:00').click()
+
+    await page.locator('text=02:00').click()
+    await page.locator('text=03:00').click()
+
+    await page.locator('text=03:00').click()
+    await page.locator('text=04:00').click()
+
+    await page.locator('text=04:00').click()
+    await page.locator('text=05:00').click()
+
+    await page.locator('text=05:00').click()
+    await page.locator('text=06:00').click()
+
+    await page.locator('text=06:00').click()
+    await page.locator('text=07:00').click()
+
+    await page.locator('text=07:00').click()
+    await page.locator('text=08:00').click()
+
+    await page.locator('text=08:00').click()
+    await page.locator('text=09:00').click()
+
+    await page.locator('text=09:00').click()
+    await page.locator('text=10:00').click()
+
+    await page.locator('text=10:00').click()
+    await page.locator('text=11:00').click()
+
+    await page.locator('text=11:00').click()
+    await page.locator('text=12:00').click()
+
+    await page.locator('text=12:00').click()
+    await page.locator('text=13:00').click()
+
+    await page.locator('text=13:00').click()
+    await page.locator('text=14:00').click()
+
+    await page.locator('text=14:00').click()
+    await page.locator('text=15:00').click()
+
+    await page.locator('text=15:00').click()
+    await page.locator('text=16:00').click()
+
+    await page.locator('text=16:00').click()
+    await page.locator('text=17:00').click()
+
+    await page.locator('text=17:00').click()
+    await page.locator('text=18:00').click()
+
+    await page.locator('text=18:00').click()
+    await page.locator('text=19:00').click()
+
+    await page.locator('text=19:00').click()
+    await page.locator('text=20:00').click()
+
+    await page.locator('text=20:00').click()
+    await page.locator('text=21:00').click()
+
+    await page.locator('text=21:00').click()
+    await page.locator('text=22:00').click()
+
+    await page.locator('text=22:00').click()
+    await page.locator('text=23:00').click()
+
+    await page.locator('text=23:00').click()
+    await page.locator('text=00:00').click()
+  })
+
+  test('should be able to change greetings text', async ({ page }) => {
+    const greetingsText = 'some beer!'
+
+    await page.locator('svg').first().click()
+    const input = page.locator('[placeholder="Have a beer"]')
+    await input.fill(greetingsText)
+    expect(input).toHaveValue(greetingsText)
+  })
+
+  test('should be able to click switcher', async ({ page }) => {
+    await page.locator('svg').first().click()
+    const modal = page.locator('[data-testid="settings-modal"]')
+    const switcher = modal.locator('role=checkbox')
+    await switcher.click()
+    expect(switcher).toBeChecked()
+    await switcher.click()
+    expect(switcher).not.toBeChecked()
   })
 })
--- playwright.config.js
+++ playwright.config.js
@@ -1,22 +1,26 @@
 const { devices } = require('@playwright/test')
 
 const DEV_SERVER_URL = 'http://localhost:3000'
-const SITE_URL = 'http://localhost:3000'
+// const SITE_URL = 'https://weekends.nac.in.ua'
 
 const config = {
   testDir: './e2e',
   timeout: 30 * 1000,
   expect: {
-    timeout: 10000,
+    timeout: 5000,
   },
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 2 : undefined,
   reporter: [['list'], ['html']],
+  globalSetup: require.resolve('./global-setup'),
   use: {
     actionTimeout: 0,
-    baseURL: SITE_URL,
+    // baseURL: SITE_URL,
+    baseURL: DEV_SERVER_URL,
     trace: 'on',
+    // trace: 'on-first-retry',
+    storageState: 'state.json',
   },
   projects: [
     {
