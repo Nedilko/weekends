dee8df52c6d1bef2c48fe61649b140e821c9feb5
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-06-15 03:30:44 +0300

    only dot reporter

--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,10 @@ export default defineConfig({
     coverage: {
       reporter: ['text', 'html'],
     },
-    minThreads: 4,
-    maxThreads: 8,
-    maxConcurrency: 20,
+    reporters: 'dot',
+    // minThreads: 4,
+    // maxThreads: 8,
+    // maxConcurrency: 20,
     exclude: [
       '**/node_modules/**',
       '**/dist/**',
