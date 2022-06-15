b6ce495e9752a101847f4b059b3f7d76035e080d
Oleksandr Nedilko <alexandr.nedilko@gmail.com>
2022-06-15 03:18:18 +0300

    added threads 8

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
     coverage: {
       reporter: ['text', 'html'],
     },
+    minThreads: 8,
+    maxThreads: 8,
     silent: true,
     exclude: [
       '**/node_modules/**',
